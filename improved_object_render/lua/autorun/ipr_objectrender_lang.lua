57511a9021838e52da58402dfd134d8755187047
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-05-08 00:01:18 +0200

    v3.1

---
+++ improved_object_render/lua/autorun/ipr_objectrender_lang.lua
@@ -0,0 +1,27 @@
+--- Script By Inj3
+--- Script By Inj3
+--- Script By Inj3
+--- https://steamcommunity.com/id/Inj3/
+if CLIENT then
+     Ipr_RenderObject = Ipr_RenderObject or {}
+
+     Ipr_RenderObject.Language = {
+          settings = "Settings panel for Improved Object Render",
+          datasend = "All data are automatically synchronized \n with players connected !",
+          enable = "Enable/Disable Module :",
+          world_dist = "World Distance :",
+          veh_dist = "Vehicle Distance :",
+          player_dist = "Player Distance :",
+          object_dist = "Object Distance :",
+          save_settings = "Save settings IOR",
+          world = "World",
+          veh = "Vehicle",
+          ply = "Player",
+          obj = "Object",
+          close_ = "Close IOR",
+          weapandmore = "Weapons on ground, doors, ragdolls, lamps, permanent objects and more..",
+          disable_all = "Disable all module",
+          enable_all = "Enable all module",
+          loading_data = "Loading data... please open the panel in a few seconds.",
+     }
+end
