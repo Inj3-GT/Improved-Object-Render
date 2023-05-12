3850956bd3d87f9a519ed717d187d54a0ddf149f
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-05-12 18:52:27 +0200

    v3.5

--- improved_object_render/lua/autorun/server/improved_object_render_sv.lua
+++ improved_object_render/lua/autorun/server/improved_object_render_sv.lua
@@ -7,101 +7,102 @@ local Ipr_Save, Ipr_Cmd = "improvedobjectrender/sauvegarde", "/objectrender"
 util.AddNetworkString("Ipr_ObjectRender_Data")
 util.AddNetworkString("Ipr_ObjectRender_P")
 
-local function Ipr_Render_FileExist(ext)
+local function Ipr_RenderExists(ext)
      local Ipr_Ext = Ipr_Save
      if (ext) then
-          Ipr_Ext = Ipr_Save ..ext
+         Ipr_Ext = Ipr_Save ..ext
      end
-     if file.Exists(Ipr_Ext, "DATA") then
-          return true
+     if (file.Exists(Ipr_Ext, "DATA")) then
+         return true
      end
-
+ 
      return false
 end
 
 local function Ipr_BroadFunc(tbl, bool, player)
      net.Start("Ipr_ObjectRender_Data")
      net.WriteTable(tbl)
-
+ 
      if (bool) then
-          net.Broadcast()
-     else
-          net.Send(player)
+         net.Broadcast()
+         return
      end
+     net.Send(player)
 end
 
 local function Ipr_SaveData(tbl, bool, player)
      local Ipr_Util = util.TableToJSON(tbl)
      file.Write(Ipr_Save.. "/sv.json", Ipr_Util)
-
+ 
      if (bool) then
-          Ipr_BroadFunc(tbl, true)
-          player:SendLua([[chat.AddText(color_red, "Improved Object Render : ", color_white, "Data has been perfectly saving and is being sent to all player !" )]])
-     else
-          MsgC(color_white, "[Improved Object Render] Success ! Data Loaded !\n" )
+         Ipr_BroadFunc(tbl, true)
+         player:SendLua([[chat.AddText(color_red, "Improved Object Render : ", color_white, "Data has been perfectly saving and is being sent to all player !" )]])
+         return
      end
-end
+ 
+     MsgC(color_white, "[Improved Object Render] Success ! Data Loaded !\n" )
+end 
 
-if not Ipr_Render_FileExist() then
+if not Ipr_RenderExists() then
      file.CreateDir(Ipr_Save)
 end
-MsgC(color_white, "[Improved Object Render] Loading Data, please wait..\n" )
-
-if not Ipr_Render_FileExist("/sv.json") then
-     MsgC( color_white, "[Improved Object Render] Creating Data, please wait..\n" )
-
-     local Ipr_Render_DefaultTbl = {
-          worldspawn = {
-               ["enable"] = true,
-               ["distance"] = 500,
-          },
-          vehicle = { 
-               ["enable"] = true,
-               ["distance"] = 1000,
-          },
-          player = {
-               ["enable"] = true,
-               ["distance"] = 1100,
-          },
-          object = {
-               ["enable"] = true,
-               ["distance"] = 800,
-          }
+if not Ipr_RenderExists("/sv.json") then
+     MsgC(color_white, "[Improved Object Render] Creating Data, please wait..\n" )
+ 
+     local Ipr_R = {
+         worldspawn = {
+             ["enable"] = true,
+             ["distance"] = 500,
+         },
+         vehicle = {
+             ["enable"] = true,
+             ["distance"] = 1000,
+         },
+         player = {
+             ["enable"] = true,
+             ["distance"] = 1000,
+         },
+         object = {
+             ["enable"] = true,
+             ["distance"] = 800,
+         }
      }
-
-     Ipr_SaveData(Ipr_Render_DefaultTbl, false)
+ 
+     Ipr_SaveData(Ipr_R, false)
 else
      MsgC(color_white, "[Improved Object Render] Success ! Data Loaded !\n" )
 end
-
+ 
 local function Ipr_Render_Cmd(ply, cmd, args)
      if not ply:IsSuperAdmin() then
          return
      end
- 
+
      net.Start("Ipr_ObjectRender_P")
      net.Send(ply)
 end
-concommand.Add("objectrender", Ipr_Render_Cmd) 
 
 local function Ipr_Rcv_Data(len, ply)
-     if ply:IsSuperAdmin() then
-          Ipr_SaveData(net.ReadTable(), true, ply)
-     else
-          ply:SendLua([[chat.AddText(color_red, "Improved Object Render : ",color_white , "You're not superadmin, where you try to send values when you're not allowed to !" )]])
+     if not ply:IsSuperAdmin() then
+         return
      end
-end
 
+     Ipr_SaveData(net.ReadTable(), true, ply)
+end
+ 
 hook.Add("PlayerSay", "Ipr_ObjectRender_Say", function(ply, text)
-     if (ply:IsSuperAdmin() and string.sub(string.lower(text), 1,  string.len(Ipr_Cmd)) == Ipr_Cmd) then
+     if not ply:IsSuperAdmin() then
+         return
+     end
+     if (string.sub(string.lower(text), 1, string.len(Ipr_Cmd)) == Ipr_Cmd) then
          net.Start("Ipr_ObjectRender_P")
          net.Send(ply)
  
          return ""
      end
-end) 
+end)
 
-hook.Add( "PlayerInitialSpawn", "Ipr_ObjectRender_Init",  function(player, transition)
+hook.Add( "PlayerInitialSpawn", "Ipr_ObjectRender_Init",  function(player)
      timer.Simple(5, function()
          if not IsValid(player) then
              return
@@ -112,4 +113,5 @@ hook.Add( "PlayerInitialSpawn", "Ipr_ObjectRender_Init",  function(player, trans
      end)
 end) 
 
+concommand.Add("objectrender", Ipr_Render_Cmd) 
 net.Receive("Ipr_ObjectRender_Data", Ipr_Rcv_Data)
