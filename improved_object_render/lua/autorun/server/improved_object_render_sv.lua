23d426adaae686ca15700d2f1a1d1076d1c4e80f
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-05-12 08:01:09 +0200

    v3.5

--- improved_object_render/lua/autorun/server/improved_object_render_sv.lua
+++ improved_object_render/lua/autorun/server/improved_object_render_sv.lua
@@ -3,6 +3,7 @@
 --- Script By Inj3
 --- https://steamcommunity.com/id/Inj3/
 local Ipr_Save, Ipr_Cmd = "improvedobjectrender/sauvegarde", "/objectrender"
+
 util.AddNetworkString("Ipr_ObjectRender_Data")
 util.AddNetworkString("Ipr_ObjectRender_P")
 
