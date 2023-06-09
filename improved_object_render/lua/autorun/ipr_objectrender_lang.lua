ecbc8d306962e2e2846107ff25c8fc2e3a4cf48c
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-06-09 07:24:09 +0200

    v3.9

--- improved_object_render/lua/autorun/ipr_objectrender_lang.lua
+++ improved_object_render/lua/autorun/ipr_objectrender_lang.lua
@@ -7,8 +7,8 @@ if (SERVER) then
 end
 Ipr_RenderObject = Ipr_RenderObject or {}
 Ipr_RenderObject.Render = Ipr_RenderObject.Render or {}
-
 Ipr_RenderObject.Version = "v3.9"
+    
 Ipr_RenderObject.Language = {
     settings = "Settings panel for Improved Object Render",
     datasend = "All data are automatically synchronized \n with players connected !",
@@ -24,4 +24,16 @@ Ipr_RenderObject.Language = {
     obj = "Prop",
     close_ = "Close IOR",
     weapandmore = "Weapons on ground, doors, ragdolls, lamps, permanent objects and more..",
+}
+Ipr_RenderObject.EntRend = {
+    ["class C_ClientRagdoll"] = "ipr_world", 
+    ["class C_BaseEntity"] = "ipr_world", 
+    ["npc_"] = "ipr_world", 
+    ["beam"] = "ipr_world", 
+    ["gmod_"] = "ipr_world", 
+    ["func_"] = "ipr_world",
+    ["prop_vehicle_"] = "ipr_vehicle", 
+    ["prop_p"] = "ipr_prop", 
+    ["prop_d"] = "ipr_prop", 
+    ["prop_r"] = "ipr_prop", 
 }
